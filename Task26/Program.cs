af81eb3f4968c29cfa685c12c54c66fa42119f94
ANTON <19111984@list.ru>
2022-08-20 21:43:05 +0700

    quantitDig

---
+++ Task26/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Написать программу, которая принимает на вход
+число и выдает кол-во цифр в числе 456 - 3, 78 - 2б, 89126 - 5*/
+Console.Clear();
+Console.WriteLine("Enter a positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+if (number < 0)
+{
+    number = - number;
+}
+int input = number;
+while (number > 0)
+{
+    number /=10;
+    i++;
+}
+Console.WriteLine($"The entered number {input} consists  of {i} digits");
