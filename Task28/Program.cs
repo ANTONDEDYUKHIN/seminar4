8e56cdc7784cd2d79b4db91f5d3526d3c785dff0
ANTON <19111984@list.ru>
2022-08-20 21:51:06 +0700

    factorial

---
+++ Task28/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Написать программу, которая принимает на вход
+число Н и выдает произведение чисел от 1 до Н
+4 - 24, 5 - 120 */
+Console.Clear();
+Console.Write("Enter a positive number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int composition = 1;
+for (int i = 1; i <= number; i++)
+{
+    composition = composition * i;
+}
+Console.WriteLine($" The product of numbers from 1 to {number} is: {composition}");
