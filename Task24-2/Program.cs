ee4359382c7c879d9eab2b4a931ef4f41c4e2bc0
ANTON <19111984@list.ru>
2022-08-20 21:17:57 +0700

    sumNumMeth

---
+++ Task24-2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Написать программу, которая принимает число А
+и выдает сумму чисел от 1 до А.
+7 - 28, 4 - 10, 8 - 36 */
+Console.Clear();
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+if (num > 0)
+{
+    int result = SumNumbers(num);
+Console.WriteLine($"Сумма чисел от 1 до {num} = {result}");
+}
+else Console.WriteLine($"Некорректный ввод значения. Требуется положительное число");
