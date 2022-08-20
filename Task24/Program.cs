1619833d3a4083354799f627f1dcb48bdab3e949
ANTON <19111984@list.ru>
2022-08-20 21:09:51 +0700

    sumNum

---
+++ Task24/Program.cs
@@ -0,0 +1,12 @@
+﻿/*Написать программу, которая принимает число А
+и выдает сумму чисел от 1 до А.
+7 - 28, 4 - 10, 8 - 36 */
+Console.Clear();
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = default; //0
+for (int i = 1; i <= num; i++)// i++ инкремент, i-- декремент
+{
+    sum = sum + i;// sum +=i
+}
+Console.WriteLine($"Сумма чисел от 1 до {num} = {sum}");
