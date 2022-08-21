b29ea8228ab5e800c78115ab56424b88c65d3a6d
ANTON <19111984@list.ru>
2022-08-21 08:49:46 +0700

    arrayRND

---
+++ Task30-1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*написать программу, которая выводит массив из 8ми
+элементов, заполненный нулями и единицами в случайном порядке*/
+Console.Clear();
+Console.Write("Enter the SIZE of the array of number: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the LARGEST number of the array: ");
+int maxDig = Convert.ToInt32(Console.ReadLine());
+int[] NewArray()
+{
+    int[] array = new int[size];
+    for (int i = 0; i <= (size-1); i++)
+    {
+        array[i] = new Random().Next(0, maxDig);
+    }
+    return array;
+}
+int [] array = NewArray();
+void PrintArray()
+{
+    for (int i = 0; i <= (size-1); i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+}
+PrintArray();
